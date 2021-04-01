693c531f35e246a0b0dbb11495f9e43061868abb
Alessandro Martinello <martinello.alessandro@gmail.com>
2021-04-01 13:41:32 +0200

    commented and reformatted

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,25 @@ import networkx as nx
 import click
 
 
-class FileNetwork():
-    def __init__(self, sourcepath, cycles_color='red', usegitignore=True):
+class FileNetwork:
+    """
+    Maps `.py` files in a folder into a network based on cross-imports.
+    Automatically highlights circular imports, and ignores folders specified in
+    `.gitignore`
+
+    Parameters:
+    ---
+    sourcepath: Path. Folder to map
+
+    cycles_color: str. Color with which to highlight edges in the network forming
+    circular imports. Can be a rgb string such as `rgba(0, 120, 20 ,1)`. Default: `'red'`
+
+    usegitignore: Bool. Whether to ignore folders specified in gitignore. Default: `True`
+    """
+
+    def __init__(self, sourcepath, cycles_color="red", usegitignore=True):
         """
+        Initializes network
         """
         self.sourcepath = Path(sourcepath)
         self.usegitignore = usegitignore
@@ -20,22 +36,36 @@ class FileNetwork():
         self.gen_network()
 
     def network_edges(self):
+        """
+        Extracts network edges
+        """
         edgelist = []
         for _file in self.filelist:
             edgelist += self._imports_into_edges(_file)
         return edgelist
 
     def network_nodes(self):
+        """
+        Extracts network nodes and their attributes
+        """
         nodes = []
         for node in self.filelist:
-            node_attributes = {'type': node.suffix}
-            if node_attributes['type'] == '.py':
-                node_attributes['size'] = 2 + log(self.get_filesize(self.sourcepath / node)) * 2
-                node_attributes['color'] = {'border': "rgba(0,70,10,1)", "background": "rgba(0, 120, 20 ,1)"}
+            node_attributes = {"type": node.suffix}
+            if node_attributes["type"] == ".py":
+                node_attributes["size"] = (
+                    2 + log(self.get_filesize(self.sourcepath / node)) * 2
+                )
+                node_attributes["color"] = {
+                    "border": "rgba(0,70,10,1)",
+                    "background": "rgba(0, 120, 20 ,1)",
+                }
             nodes.append((self.name(node), node_attributes))
         return nodes
 
     def gen_network(self):
+        """
+        Construct the Directed Graph representing cross-imports
+        """
         di = nx.DiGraph()
         di.add_edges_from(self.network_edges())
         di.add_nodes_from(self.network_nodes())
@@ -44,15 +74,20 @@ class FileNetwork():
         return self
 
     def highlight_cycles(self):
+        """
+        Finds circular imports in the network
+        """
         for cycle in nx.simple_cycles(self.network):
             i = 0
             while i < len(cycle) - 1:
-                self.network[cycle[i]][cycle[i + 1]]['color'] = self.cycles_color
+                self.network[cycle[i]][cycle[i + 1]]["color"] = self.cycles_color
                 i += 1
-            self.network[cycle[-1]][cycle[0]]['color'] = self.cycles_color
+            self.network[cycle[-1]][cycle[0]]["color"] = self.cycles_color
 
     def _imports_into_edges(self, filepath):
         """
+        Loops through python files in the folder and transforms import
+        statements into edges
         """
         edgelist = []
         for imp in self.get_imports(self.sourcepath / filepath):
@@ -63,90 +98,120 @@ class FileNetwork():
                     internal_file = True
                     break
             if internal_file is False:
-                imp = imp.split('.')[0]
+                imp = imp.split(".")[0]
                 edge_dictionary = {}
             else:
-                edge_dictionary = {'width': 4}
+                edge_dictionary = {"width": 4}
             edgelist.append((imp, self.name(filepath), edge_dictionary))
         return edgelist
 
     @staticmethod
-    def _get_pyfilelist(srcpath, usegitignore=True):
+    def _get_pyfilelist(srcpath, usegitignore=True) -> list:
         """
+        Gets files in `srcpath` to represent in a network
         """
-        gitignorefile = srcpath / Path('.gitignore')
+        gitignorefile = srcpath / Path(".gitignore")
         if usegitignore and gitignorefile.exists():
             with gitignorefile.open() as f:
                 lines = f.read().splitlines()
             gitignore = [
-                Path(line) for line in lines if not line.strip().startswith('#') and len(line.strip()) > 1 and Path(line).suffix == ''
-            ] + [Path('.git')]
-            viablepaths = [p for p in srcpath.glob('*/') if p.is_dir() and p not in gitignore]
-            filelist = set().union(*[set(p.glob('**/*.py')) for p in viablepaths])
+                Path(line)
+                for line in lines
+                if not line.strip().startswith("#")
+                and len(line.strip()) > 1
+                and Path(line).suffix == ""
+            ] + [Path(".git")]
+            viablepaths = [
+                p for p in srcpath.glob("*/") if p.is_dir() and p not in gitignore
+            ]
+            filelist = set().union(*[set(p.glob("**/*.py")) for p in viablepaths])
         else:
-            filelist = srcpath.glob('**/*.py')
+            filelist = srcpath.glob("**/*.py")
 
         return [p.relative_to(srcpath) for p in filelist]
 
     @staticmethod
-    def get_imports(inputfile):
+    def get_imports(inputfile) -> list:
         """
+        Finds iport statements in a `.py` file
         """
         with inputfile.open() as f:
             lines = f.read().splitlines()
 
         lines = [line for line in lines if len(line.strip()) > 0]
         importlist = [
-            iline.split()[1] for iline in [
-                line for line in lines if (line.strip().split()[0] == 'import') or (line.strip().split()[0] == 'from')
+            iline.split()[1]
+            for iline in [
+                line
+                for line in lines
+                if (line.strip().split()[0] == "import")
+                or (line.strip().split()[0] == "from")
             ]
         ]
         return importlist
 
     @staticmethod
-    def get_filesize(inputfile):
-        with open(inputfile, 'rb') as f:
+    def get_filesize(inputfile) -> int:
+        """
+        Returns the legth of a file in lines
+        """
+        with open(inputfile, "rb") as f:
             lines = 0
             buf_size = 1024 * 1024
             read_f = f.raw.read
 
             buf = read_f(buf_size)
             while buf:
-                lines += buf.count(b'\n')
+                lines += buf.count(b"\n")
                 buf = read_f(buf_size)
 
         return lines
 
     @staticmethod
     def name(path):
-        return str(path).replace('/', '.').replace('.py', '')
+        return str(path).replace("/", ".").replace(".py", "")
 
 
 def get_pyvis_options():
-    with open('src/pyvis_options.json', 'r') as f:
+    with open("src/pyvis_options.json", "r") as f:
         options = f.read()
     return f"""var options = {str(options)}"""
 
 
-def plot_network(path, saveas=None):
+def plot_network(path, saveas=None, **kwargs):
+    """
+    Maps `.py` files in a folder into a network based on cross-imports and plots it.
+    Automatically highlights circular imports, and ignores folders specified in
+    `.gitignore`
+
+    Parameters:
+    ---
+    path: Path. Folder to map
+
+    saveas: str. Html file for the network representation
+
+    **kwargs: Keyword arguments passed to `FileNetwork()`
+    """
     if saveas is None:
-        saveas = '_srcnetwork.html'
-    fn = FileNetwork(path)
-    nt = Network('1500px', '1500px')
+        saveas = "_srcnetwork.html"
+    fn = FileNetwork(path, **kwargs)
+    nt = Network("1500px", "1500px")
     nt.toggle_physics(True)
     nt.from_nx(fn.network)
     nt.set_options(get_pyvis_options())
-    nt.show(f'{saveas}.html')
+    nt.show(f"{saveas}")
     return
 
 
 @click.command()
-@click.argument('path', type=click.Path(exists=True))
-@click.option('--saveas', default='_scrnetwork.html', show_default=True, type=click.File(mode='w'))
+@click.argument("path", type=click.Path(exists=True))
+@click.option(
+    "--saveas", default="_scrnetwork.html", show_default=True, type=click.File(mode="w")
+)
 def plotnetwork(path, saveas):
     click.echo(path)
     click.echo(saveas)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     plotnetwork()
